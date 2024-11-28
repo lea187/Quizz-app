335452154584d6136d4fd53f00b08e3b5f96db3c
Lea Gomis <lea.187@hotmail.fr>
2024-11-28 14:16:57 +0100

    essai quiz sans api

---
+++ quiz-app.py
@@ -0,0 +1,107 @@
+welcome = "Bienvenue sur mon Quiz! "
+print(welcome)
+
+playing = input ("Je te donne le nom d'un personnage et tu dois retrouver le nom de la série. Veux-tu y jouer ? ")
+if playing.lower() != "oui" and playing.lower() != "ok" :
+    quit()
+
+print("Ok, c'est parti ! :)")
+
+reponse = input("Owen Hunt : ")
+if reponse.lower() == "grey's anatomy":
+    print("Bonne réponse !")
+elif reponse.lower() == "greys anatomy":
+    print("Bonne Réponse ! ")
+else: print("Mauvaise réponse ! ") 
+
+reponse = input("rajesh Koothrapoli : ")
+if reponse.lower() == "the big bang theory":
+    print("Bonne réponse !")
+elif reponse.lower() == "big bang theory":
+    print("Bonne Réponse ! ")
+else: print("Mauvaise réponse ! ") 
+
+reponse = input("samwell tarly : ")
+if reponse.lower() == "game of thrones":
+    print("Bonne réponse !")
+elif reponse.lower() == "games of throne":
+    print("Bonne Réponse ! ")
+elif reponse.lower() == "game of throne":
+    print("Bonne Réponse ! ")
+elif reponse.lower() == "games of thrones":
+    print("Bonne Réponse ! ")
+else: print("Mauvaise réponse ! ") 
+
+reponse = input("Kady Kyle : ")
+if reponse.lower() == "ma famille d'abord":
+    print("Bonne réponse !")
+elif reponse.lower() == "ma famille dabord":
+    print("Bonne Réponse ! ")
+else: print("Mauvaise réponse ! ") 
+
+reponse = input("eddie thomas : ")
+if reponse.lower() == "phénomène raven":
+    print("Bonne réponse !")
+elif reponse.lower() == "phenomène raven":
+    print("Bonne Réponse ! ")
+elif reponse.lower() == "phénomene raven":
+    print("Bonne Réponse ! ")
+elif reponse.lower() == "phenomene raven":
+    print("Bonne Réponse ! ")
+else: print("Mauvaise réponse ! ") 
+
+reponse = input("stevie : ")
+if reponse.lower() == "malcolm":
+    print("Bonne réponse !")
+elif reponse.lower() == "malcolme":
+    print("Bonne Réponse ! ")
+else: print("Mauvaise réponse ! ") 
+
+reponse = input("London Tipton : ")
+if reponse.lower() == "la vie de palace de zack et cody":
+    print("Bonne réponse !")
+elif reponse.lower() == "la vie de palace de zack et kody":
+    print("Bonne réponse !")
+elif reponse.lower() == "vie de palace de zack et cody":
+    print("Bonne Réponse ! ")
+elif reponse.lower() == "vie de palace de zac et cody":
+    print("Bonne Réponse ! ")
+elif reponse.lower() == "la vie de palace":
+    print("Bonne Réponse ! ")
+elif reponse.lower() == "zack et cody":
+    print("Bonne Réponse ! ")
+elif reponse.lower() == "zac et cody":
+    print("Bonne Réponse ! ")
+elif reponse.lower() == "zack et kody":
+    print("Bonne Réponse ! ")
+else: print("Mauvaise réponse ! ") 
+
+reponse = input("Eddie Britt : ")
+if reponse.lower() == "desperate housewives":
+    print("Bonne réponse !")
+elif reponse.lower() == "desperates housewives":
+    print("Bonne Réponse ! ")
+elif reponse.lower() == "desperate housewive":
+    print("Bonne Réponse ! ")
+elif reponse.lower() == "desperates housewive":
+    print("Bonne Réponse ! ")
+else: print("Mauvaise réponse ! ") 
+
+reponse = input("Peyton : ")
+if reponse.lower() == "les frères scott":
+    print("Bonne réponse !")
+elif reponse.lower() == "les freres scott":
+    print("Bonne Réponse ! ")
+elif reponse.lower() == "freres scott":
+    print("Bonne Réponse ! ")
+elif reponse.lower() == "frere scott":
+    print("Bonne Réponse ! ")
+else: print("Mauvaise réponse ! ") 
+
+reponse = input("Lana Lang : ")
+if reponse.lower() == "smallville":
+    print("Bonne réponse !")
+elif reponse.lower() == "small ville":
+    print("Bonne Réponse ! ")
+else: print("Mauvaise réponse ! ") 
+
